18ff978f9763afa19725d523e5936728bf2cb3d8
surendr-Reddy <surendr.8reddy@gmail.com>
2024-02-11 15:01:59 +0530

    swiggy- hardcoded ResCard

--- App.js
+++ App.js
@@ -1,75 +1,103 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDom from "react-dom/client";
+import logo from './images/logo.jpg';
 
-// Using React.createElement to create elements
-const content = React.createElement("h1", {}, "Welcome to React with npm");
-const child = React.createElement("div", { id: "child" });
-const heading = React.createElement("div", { id: "parent" }, [child, content]);
+import healthyStreet from './images/healthy-food.jpg';
 
-console.log(React);
+/**
+  HEADER
+   -logo
+   -nav bar
+BODY
+   -search
+   -restraurantContainer
+    -restraurant-card
+     -logo
+     -name
+     -startrating
+     -delivary-time
+   -restaurantCards
+FOOTOER
+  -copyrigth 
+  -socialmedialinks
+  -adress 
+  -contanct 
+  **/
+ const heading =React.createElement("div",{},"welcome to javascript");
+ const headinge=<div>hi</div>
+const Heading = ()=><><div> hihi</div><div> hihi</div></>
 
-// JSX creating elements code
-const jsxHeading = <h1 className="classNameShouldBeCamelCase">This is From JSX Element</h1>;
 
-// JSX is not HTML; it has a syntax similar to HTML.
-// Babel will convert JSX into React code, and then React will render it using the ReactDOM.render method.
-
-// JSX (transpiled before it reaches the JS engine by Parcel and Babel)
-// JSX => React.createElement (Babel does this) ==> ReactElement object ===> HTML element
-
-// Using ReactDOM.createRoot to render the JSX element
-// If it is multiple lines, wrap it in parentheses ()
-const jsxHeading2 = (
-  <div>
-    <div>
-      <h1>Welcome to React</h1>
+const Header =()=>{
+    return (
+<div className="header">
+     <div className="logoContainer">
+        <img className="logo" src={logo} alt="Logo"/>
     </div>
-  </div>
-);
-
-// React component
-// Class-based component
-// Functional-based component
-
-// React element
-const reactElement = <div>This is a react element</div>;
-const FunctionalComp2 = ()=><div>from functional component2</div>;
-// Functional-based component
-// The functional component should return the JSX
-const FunctionalComp = () => 
-
-<div> 
-{/* //if wnat to run react elemet use{} bcz any of the js code we can write within in the jsx by {} */}
-<FunctionalComp2/>
-<h1>Hi from functional component</h1>{reactElement}{FunctionalComp2()}
-<FunctionalComp2> </FunctionalComp2>
-
- {/* // we can call the function by {FunctionalComp2()} */}
- 
+ <div className="nav-items">
+  <ul>
+     <li>Home</li>
+     <li>About</li>
+     <li>contact Us</li>
+     <li>Cart</li>
+</ul>
+    </div>
 </div>
-
-// If we are calling a functional component inside another functional component, that is called component composition
-
-
-const a= <FunctionalComp2> </FunctionalComp2>
-// 3 types of declaring functions
-
-// 1. Normal function
-// name(2, 3);
-function sum(a, b) {
-  return a + b;
+    )
 }
 
-// 2. Function expression
-const sumExpression = function (a, b) {
-  return a + b;
-};
+const RestraurantCard = ()=>{
+    return(
+        
+<div className="res-card" >
+     <img className="res-logo"  src={healthyStreet}/>
+     
+    <div className="resName"><h2>HealthyStreet</h2></div>
+    <div className="resName"> <h2>salad,sprouts</h2></div>
+    <div className="start-rating"><h4>Rating :4.9 rating</h4></div>
+    <div className="delivery"><h4>Delivery Time:45min</h4></div>
+</div>
+        
 
-// 3. Arrow function
-const sumArrow = (a, b) => a + b;
+    )
+}
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<FunctionalComp/>); 
-root.render(<FunctionalComp2> </FunctionalComp2>); 
-root.render(a);
-root.render(FunctionalComp());
+const Body =()=>{
+    return (
+        <div className="restraurant-contaniner">
+        <div className="search-bar"><h3>Serach-bar</h3></div>
+        <div className="restraurant-card">
+            <RestraurantCard/>
+            <RestraurantCard/>
+            <RestraurantCard/>
+            <RestraurantCard/>
+            <RestraurantCard/>
+            <RestraurantCard/>
+            <RestraurantCard/>
+            <RestraurantCard/>
+            <RestraurantCard/>
+            <RestraurantCard/>
+            <RestraurantCard/>
+            <RestraurantCard/>
+            <RestraurantCard/>
+            <RestraurantCard/>
+            <RestraurantCard/>
+            <RestraurantCard/>
+
+        </div>
+        </div>
+    )
+}
+
+ 
+
+const App =() => {
+    return (<>
+<Header/>
+<Body/>
+</>
+    )
+}
+
+ const root =ReactDom.createRoot(document.getElementById("root"));
+ root.render(<App/>);
